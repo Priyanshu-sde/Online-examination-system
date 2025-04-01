302525d11c1097847da59bff1a3697814efa572b
Priyanshu <priyanshu.sde@gmail.com>
2025-04-01 19:22:05 +0530

    completed signup endpoint

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const user = require("./routes/users.js");
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://priyanshusde:EeLDkYLEoBzsp7Tt@cluster0.kp9pk.mongodb.net/exam-system");
+
+app.use("/user",user)
+
+app.listen(3000, () => {
+    console.log("server started");
+})
